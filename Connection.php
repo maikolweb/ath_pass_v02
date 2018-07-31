fef000696eaa681700663d5205776eb700beca4a
maikolweb <41301485+maikolweb@users.noreply.github.com>
2018-07-31 17:25:48 -0500

    se actualiza clase coneccion
    se agrega datos de coneccion

--- Connection.php
+++ Connection.php
@@ -1,35 +1,32 @@
 <?php
-class Connection
-{
-    private $numero=1;
-    private $numero2=2;
+ini_set("display_errors", 1); 
 
-
-  function __construct()
-  {
-
-  }
-
-  public function getNumero(){
-    return $this-> numero;
-  }
-  public function setNumero($numero){
-    $this-> numero= $numero;
-  }
-  public function getNumero2(){
-    return $this-> numero2;
-  }
-  public function setNumero2($numero2){
-    $this-> numero2= $numero2;  }
-
-public function suma(){
-
-  $result=$this-> numero + $this-> numero2;
-  return $result;
+class Connection {
+	private static $instance= null;
+	private $userName;
+	private $password;
+	private $host;
+	private $db;
+	
+	public function __construct (){
+		//Se llama el archivo donde estan las constantes de BD
+		require_once '../config/database.php';
+		
+		$this-> userName =USERNAME;
+		$this-> 
+		
+		
+	}
+	public function getUserName(){
+		return $this-> numero;
+	}
+	public function setUserName($userName){
+		$this-> userName= $userName;
+	}
+	
+	public function prueba(){
+		
+		
+		echo 'usuario : '.USERNAME;
+	}
 }
-
-}
-
-$objecto= new Connection();
-
-echo 'hola'.$objecto->suma();
